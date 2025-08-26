d07f404d311a32e02ef312a4348c422b2cc3e6fd
Gabriel Vargas <vmgabriel96@gmail.com>
2025-08-25 21:54:44 -0500

    feat: add tailscale installation

--- base.sh
+++ base.sh
@@ -3,3 +3,6 @@ sudo apt update && sudo apt upgrade -y
 
 echo "📥 Instalando Emacs"
 sudo apt-get install -y emacs-nox
+
+echo "Instalar Tailscale"
+curl -fsSL https://tailscale.com/install.sh | sudo bash
